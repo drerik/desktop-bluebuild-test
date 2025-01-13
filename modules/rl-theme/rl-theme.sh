407019e10522a8a87d2de5ad7cf9774f118afd1b
Erik Kaareng-Sunde <erik@eriksunde.com>
2025-01-13 15:30:37 +0100

    fixed rl-theme default linking

--- modules/rl-theme/rl-theme.sh
+++ modules/rl-theme/rl-theme.sh
@@ -8,5 +8,8 @@ mkdir /usr/share/backgrounds/rl
 cp ${MODULE_DIRECTORY}/rl-theme/files/rl.xml /usr/share/backgrounds/rl/rl.xml
 cp ${MODULE_DIRECTORY}/rl-theme/files/default.png  /usr/share/backgrounds/rl/default.png
 #Setting rl background to default
-ls -s  /usr/share/backgrounds/rl/default.png /usr/share/backgrounds/default.png
-ls -s  /usr/share/backgrounds/rl/rl.xml /usr/share/backgrounds/default.xml
+rm /usr/share/backgrounds/default.png
+rm /usr/share/backgrounds/default.xml
+ln -s  /usr/share/backgrounds/rl/default.png /usr/share/backgrounds/default.png
+ln -s  /usr/share/backgrounds/rl/rl.xml /usr/share/backgrounds/default.xml
+
